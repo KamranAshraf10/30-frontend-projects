efac83c60e80c6d7ad38d60e6b556f0e89e1b66d
KamranAshraf10 <kamranashraf970920@gmail.com>
2025-06-27 16:10:49 +0500

    Updated expanding cards project

--- 01-Expanding-Cards/script.js
+++ 01-Expanding-Cards/script.js
@@ -1,9 +1,6 @@
 const panels = document.querySelectorAll(".panel");
 
-// console.log(panels);
-
 panels.forEach((panel) => {
-  //   console.log(panel);
   panel.addEventListener("click", () => {
     removeActiveClasses();
     panel.classList.add("active");
