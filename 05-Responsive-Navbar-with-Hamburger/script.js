e061e5efaed0359c615712b0cd0d551e3a7b7c11
KamranAshraf10 <kamranashraf970920@gmail.com>
2025-07-18 22:48:21 +0500

    Responsive-Navbar-with-Hamburger-Menu

---
+++ 05-Responsive-Navbar-with-Hamburger/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const $hamburger = $('.hamburger');
+    const $navlinks = $('.nav-links');
+
+    $hamburger.click(function() {
+        const expanded = $(this).attr('data-expanded') === 'true';
+
+        $(this).attr('data-expanded', !expanded);
+        $navlinks.attr('data-active', !expanded);
+    });
+
+    $('.nav-links').click(function () {
+        $hamburger.attr('data-expanded', false);
+        $navlinks.attr('data-active', false);
+    });
+});
